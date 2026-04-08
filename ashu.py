8c2d89b2a3021888702b6e08383211bf48553924
ashutosh raj <ashutoshraj9065@gamil.com>
2026-04-08 21:44:29 +0530

    practice

---
+++ ashu.py
@@ -0,0 +1,9 @@
+# x=10
+# print(x)
+# x=10
+
+# print(x>>2)
+
+x=10
+y=10
+print(id(x),id(y))
---
+++ practice.py
@@ -0,0 +1,32 @@
+
+import string
+import keyword
+
+print(len(keyword.kwlist))
+print(keyword.kwlist)
+print(len(keyword.softkwlist))
+
+print(string.punctuation)
+print(len(string.punctuation))
+
+x=10
+y=5
+print(x/y)
+print(x//y)
+print(x%y)
+
+print(x:="python")
+
+x=20
+y=10
+z=30
+print(x==y)
+print(x>y)
+
+print(x>y or y>z)
+x=[10,20]
+y=[10,20]
+print(x is y)
+
+s="i am a good boy"
+print("am" in s)

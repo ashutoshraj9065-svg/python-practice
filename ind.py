86e4f2f262fecf2e7117f2cf132bbbc04a904456
ashutosh raj <ashutoshraj9065@gamil.com>
2026-04-12 14:30:03 +0530

    update

--- ind.py
+++ ind.py
@@ -4,10 +4,22 @@
 # print(s.index("s",2,7))
 # print(s.index("s",))
 
-s="programmging"
+# s="programmging"
 # print(s.index("g",2,-7,5))
 # print(s.index("g"))
 # print(s.index("g",4,9))
-print(s.index("i",3))
-print(s.index("r",2,8))
+# print(s.index("i",3))
+# # print(s.index("r",2,8))
+# list = [1, 2, 3, 4, 50, 40, 30, 50, 9, 10]
+# element = 50
+# print(list.index(element,5))
+
+
+
+lst= list(map(int,input("enter a list number").split()))
+user=int(input("enter a number"))
+if(user in lst):
+    print(lst.index(user))
+else:
+    print("not found")
 

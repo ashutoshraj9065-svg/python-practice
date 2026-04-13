b50117b10c55c3436d94304ff166217e10e30f6b
ashutosh raj <ashutoshraj9065@gamil.com>
2026-04-13 20:32:38 +0530

    python end and sep

---
+++ mystr.py
@@ -0,0 +1,22 @@
+print('this is "python"')
+# print('this is 'python'')
+print("this is 'java'")
+# print('this is 'java'')
+
+print("this is \"c\"")
+print('this is \'c++\'')
+print("hello ")
+# iska output ayga space leke bt hame ye v dikhana hai space ki jagah coma kar skte hai
+
+print(10,20,30)
+print(10,20,30, sep=",")
+
+# hello or world dono ko same line me ham la skte hai python me v print hr output ke last me new line add karta hai isliye next print next line se start hota hai
+print("hello")
+print("world")
+
+print("hello" ,end=" ")
+print("world")
+
+print("this is 'a' pyhton")
+print("this is \"a\"pyhton")

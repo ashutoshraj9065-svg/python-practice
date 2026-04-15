4c04d805c9358a0f87e2fdcac78f6bc6ce984eaa
ashutosh raj <ashutoshraj9065@gamil.com>
2026-04-15 13:45:45 +0530

    update

--- my_str.py
+++ my_str.py
@@ -1,23 +1,33 @@
-s = "ThisIspythonclass"
-print(s.lower()) #this is python class convert to lower case
-print(s.upper())
-print(s.swapcase())
-print(s.title())
-print(s.capitalize())
-#count
-print(s.count("i"))
-print(s.count("I"))
-print(s.count("s"))
-#startswith
-print(s.startswith("T"))
-#split
-print(s.split( "s"))
-#join
-s1="java"
-s2="c"
-s3="node"
-print("-".join((s1,s2,s3)))
+# s = "ThisIspythonclass"
+# print(s.lower()) #this is python class convert to lower case
+# print(s.upper())
+# print(s.swapcase())
+# print(s.title())
+# print(s.capitalize())
+# #count
+# print(s.count("i"))
+# print(s.count("I"))
+# print(s.count("s"))
+# #startswith
+# print(s.startswith("T"))
+# #split
+# print(s.split( "s"))
+# #join
+# s1="java"
+# s2="c"
+# s3="node"
+# print("-".join((s1,s2,s3)))
 
-#is alpha
-print(s3.islower())
-#strip not use 
+# #is alpha
+# print(s3.islower())
+# #strip not use 
+
+
+ashu="this is a core c class"
+print(ashu.capitalize())
+print(ashu.title())
+print(ashu.lower())
+print(ashu.count("i"))
+print(ashu.isalpha())
+print(ashu.split("s"))
+print(ashu.startswith("T"))

e90946dfd6445f37b8e796218661057a22dce90c
ashutosh raj <ashutoshraj9065@gamil.com>
2026-04-19 13:50:02 +0530

    setcode

--- revise.py
+++ revise.py
@@ -1,4 +1,20 @@
-l=[10,20,"pyhton","java"]
-# l.append("php")
-l.extend([100,200])
-print(l)
+# l=[10,20,"pyhton","java"]
+# # l.append("php")
+# l.extend([100,200])
+# print(l)
+
+ashu={
+    'name':'ashu',
+     'age':19
+}
+print(ashu)
+# d1=ashu.copy()
+# print(d1)
+# banty=ashu.fromkeys('city':'indore')
+# print(banty)
+b=ashu.get('name')
+print(b)
+c=ashu.items()
+print(c)
+
+
